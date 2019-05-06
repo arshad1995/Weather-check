be105b59281dc1ab28a8b8162f5a30502cc4b84e
arshad1995 <arshadarshu445@gmail.com>
2019-05-06 15:27:58 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ import Weather from "./Weather";
 class App extends Component {
   state = {
     weather: {},
-    input: ""
+    input: "",
+    error: ""
   };
 
   api = nam => {
@@ -22,15 +23,18 @@ class App extends Component {
     )
       .then(data => data.json())
       // .then(data => console.log(data))
-      .then(data => this.setState({ weather: data }));
+      .then(data => this.setState({ weather: data, error: data.message }));
   };
-  search = async () => {
+  // search = async () => {
+  search = () => {
     // await this.setState({ name: this.state.input });
     // console.log("name", this.state.input);
+
     this.api(this.state.input);
   };
 
   render() {
+    console.log(this.state.weather);
     return (
       <div className="app">
         {/* {console.log(
@@ -38,18 +42,52 @@ class App extends Component {
             this.state.weather.main && name: ""
             this.state.weather.main.pressure
         )} */}
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        />
         <div className="input">
           <input
             type="text"
+            placeholder="search..."
             onChange={a => this.setState({ input: a.target.value })}
           />
-          <button onClick={this.search}>search</button>
-          <p className="cloud">
+          <button onClick={this.search}>
+            <i class="fa fa-search" />
+          </button>
+          <p>city:{this.state.weather && this.state.weather.name}</p>
+          <p>
             {this.state.weather &&
               this.state.weather.weather &&
               this.state.weather.weather.map(b => b.description)}
           </p>
+          <p>
+            long:
+            {this.state.weather &&
+              this.state.weather.coord &&
+              this.state.weather.coord.lon}
+          </p>
+          <p>
+            lat:
+            {this.state.weather &&
+              this.state.weather.coord &&
+              this.state.weather.coord.lat}
+          </p>
+          wind
+          <p>
+            speed:
+            {this.state.weather &&
+              this.state.weather.wind &&
+              this.state.weather.wind.speed}
+          </p>
+          <p>
+            deg:
+            {this.state.weather &&
+              this.state.weather.wind &&
+              this.state.weather.wind.deg}
+          </p>
         </div>
+
         <Weather
           pressure={
             this.state.weather &&
@@ -67,15 +105,16 @@ class App extends Component {
             this.state.weather.main.temp
           }
           pressureimg={
-            "https://cdn4.iconfinder.com/data/icons/energy-and-power-2-5/512/63-512.png"
+            "https://cdn1.iconfinder.com/data/icons/power-industry/60/control_panel_icon_meter_indicator_level_gauge_pressure_counter-512.png"
           }
           humidityimg={
-            "https://previews.123rf.com/images/nexusby/nexusby1610/nexusby161000019/63492133-humidity-icon-logo-element.jpg"
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1gROLsYJKSBGj_1DNCAl6Rb0avMnU3ov7AlDV_3NCuWG66d7r"
           }
           temperatureimg={
             "https://cdn1.iconfinder.com/data/icons/weather-filled-outline-1/50/Weather_Filled_Outline-21-512.png"
           }
         />
+        <p className="err">{this.props.error}</p>
       </div>
     );
   }
--- src/Weather.js
+++ src/Weather.js
@@ -1,45 +1,24 @@
 import React, { Component } from "react";
 import "./Weather.css";
-// const api_key = "73a30d1298cbe84369f1de28b5942918";
-// const link = `http://api.openweathermap.org/data/2.5/weather?q=${
-//   this.state.name
-// }&APPID=${api_key}`;
 
 class Weather extends Component {
-  // state = {
-  //   input: "",
-  //   name: ""
-  // };
-  // search = () => {
-  //   this.setState({ name: this.state.input });
-  // };
   render() {
     return (
       <div className="main">
-        {/* <div className="input">
-          <input
-            type="text"
-            onChange={a => this.setState({ input: a.target.value })}
-          />
-          <button onClick={this.search}>search</button>
-          <p>{this.state.name}</p>
-        </div> */}
-        {/* <div className="all"> */}
         <div className="pressure">
           <img src={this.props.pressureimg} />
-          <p> {this.props.pressure}</p>
+          <p> {this.props.pressure}kpa</p>
         </div>
 
         <div className="temperature">
           <img src={this.props.temperatureimg} />
-          <p> {this.props.temperature}</p>
+          <p> {this.props.temperature}F</p>
         </div>
 
         <div className="humidity">
           <img src={this.props.humidityimg} />
-          <p> {this.props.humidity}</p>
+          <p> {this.props.humidity}%</p>
         </div>
-        {/* </div> */}
       </div>
     );
   }
